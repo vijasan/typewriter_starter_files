a09652df5def4c34e7a526dd7424596444da8f05
Vijasan Vasantharajan <vijasan.v@hotmail.com>
2023-02-07 13:47:25 +0100

    a little clean up

--- typewriter.js
+++ typewriter.js
@@ -1,46 +1,40 @@
-// Select the target element with the id "typewriter"
+// Select the target element
 const target = document.querySelector("#typewriter");
 
-// Get the innerHTML of the target element
+// Get the innerHTML of the target
 const text = target.innerHTML;
 
-// Set the innerHTML of the target element to an empty string
+// Reset the innerHTML of the target
 target.innerHTML = "";
 
-// Select audio elements with id "typekey1" and "typekey2"
+// Select audio elements
 const typekey1 = document.querySelector("#typekey1");
 const typekey2 = document.querySelector("#typekey2");
-
-// Select audio element with id "typespace"
 const typespace = document.querySelector("#typespace");
 
-// Initialize index for text string
+// Initialize index for text
 let i = 0;
 
 // Define the typeWriter function
 const typeWriter = () => {
-  // If there is still text to be typed
+  // Continue typing if there is text left
   if (i < text.length) {
-    // Check if the current character is a space
+    // Play audio based on character
     if (text.charAt(i) === " ") {
-      // If so, reset the audio and play it
       typespace.currentTime = 0;
       typespace.play();
     } else {
-      // Otherwise, choose one of the typekey audio elements randomly
       const typekey = Math.random() < 0.5 ? typekey1 : typekey2;
-      // Reset the audio and play it
       typekey.currentTime = 0;
       typekey.play();
     }
-    // Append the current character to the target element
+
+    // Append the current character
     target.innerHTML += text.charAt(i);
-    // Increase the index
     i++;
-    // Call the typeWriter function after 50 milliseconds
     setTimeout(typeWriter, 50);
   }
 };
 
-// Invoke the typeWriter function to start the animation
+// Start the animation
 typeWriter();
